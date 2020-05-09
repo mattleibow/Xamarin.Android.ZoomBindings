7a496a36879254cb8a562f3542c2caa48b5ed1a0
Minghao Li <liminghao0922@hotmail.com>
2020-05-09 12:23:59 +0900

    fix build errors

---
+++ ZoomUtils/AndroidAppUtil.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Java.Interop;
+
+namespace US.Zoom.Androidlib.Util
+{
+    partial class AndroidAppUtil
+    {
+        partial class ResolbeInfoComparator
+        {
+
+            int Java.Util.IComparator.Compare(Java.Lang.Object o1, Java.Lang.Object o2)
+            {
+                return this.Compare((global::Android.Content.PM.ResolveInfo)o1, (global::Android.Content.PM.ResolveInfo)o2);
+            }
+        }
+    }
+}
